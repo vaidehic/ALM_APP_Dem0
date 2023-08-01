08bdc80a69d2d12c235c23262c1af4ab16d079dc
vaidehic <vaidehic@cybage.com>
2023-08-01 17:21:43 +0530

    Initial Commit

---
+++ ALM APP Dem0/ALM_APP_Dem0App.swift
@@ -0,0 +1,17 @@
+//
+//  ALM_APP_Dem0App.swift
+//  ALM APP Dem0
+//
+//  Created by administrator on 01/08/23.
+//
+
+import SwiftUI
+
+@main
+struct ALM_APP_Dem0App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
